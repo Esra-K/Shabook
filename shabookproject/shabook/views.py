1fa2ea322553c839ac06fc4155531fee0dc7ff16
Esra-K <esra.ka170@gmail.com>
2019-06-27 00:03:30 +0430

    Add files via upload

---
+++ shabookproject/shabook/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from shabook.models import Post
+# Create your views here.
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'shabook/home.html', context)
+
+
+def landing(request):
+    return render(request, 'shabook/about.html', {'title': 'About'})
